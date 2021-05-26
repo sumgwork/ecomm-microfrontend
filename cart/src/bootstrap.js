c83135271fa5693b48048aee575d8d848cff9cab
Sumit Govil <sumg.work@gmail.com>
2021-05-26 16:46:06 +1000

    use custom element for mount

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,15 @@
 import faker from "faker";
 
-const cart = `<h3>Congratulations! You have ${faker.datatype.number()} products in your cart.</h3>`;
+const mount = (el) => {
+  const cart = `<h3>Congratulations! You have ${faker.datatype.number()} products in your cart.</h3>`;
+  el.innerHTML = cart;
+};
 
-document.querySelector("#dev-cart").innerHTML = cart;
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart");
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,2 +1,5 @@
-import "products/ProductsIndex";
-import "cart/CartIndex";
+import { mount as mountProducts } from "products/ProductsIndex";
+import { mount as mountCart } from "cart/CartIndex";
+
+mountProducts(document.querySelector("#my-products"));
+mountCart(document.querySelector("#my-cart"));
